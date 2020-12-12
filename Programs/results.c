b295203dee481377a1451fb6515d7393c26a4d95
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2020-12-12 17:42:35 +0100

    Table 2 and 3 formatted

--- Programs/results.c
+++ Programs/results.c
@@ -12,10 +12,10 @@ void printResult1(int amount_of_processes, process processes[]){
 void printResult2(int amount_of_processes, process processes[], manufacturing_system manu_system){
     int i;
     double availability, performance, quality, OEE;
-    printf("\n______________________________________________________________\n");
-    printf(ANSI_UNDERLINED_PRE "|                            Table 2                         |" ANSI_UNDERLINED_POST "\n");
+    printf("\n__________________________________________________________________\n");
+    printf(ANSI_UNDERLINED_PRE "|                            Table 2                             |" ANSI_UNDERLINED_POST "\n");
 
-    printf(ANSI_UNDERLINED_PRE "| Process | OEE |  Availability  |  Performance  |  Quality  |" ANSI_UNDERLINED_POST "\n");
+    printf(ANSI_UNDERLINED_PRE "| Process |   OEE   |  Availability  |  Performance  |  Quality  |" ANSI_UNDERLINED_POST "\n");
     
     for(i = 0; i < amount_of_processes; i++) {
         availability = calculateAvailability(manu_system.planned_production_time - stops(processes[i], amount_of_processes), manu_system);
@@ -23,7 +23,7 @@ void printResult2(int amount_of_processes, process processes[], manufacturing_sy
         quality = calculateQuality(manu_system.total_count - defects(processes[i], amount_of_processes), manu_system);
         OEE = calculateOEE1(availability, performance, quality);
 
-        printf(ANSI_UNDERLINED_PRE "| %3d | %3.3f | %3.3f | %3.3f | %3.3f |" ANSI_UNDERLINED_POST "\n", i+1, OEE, availability, performance, quality);
+        printf(ANSI_UNDERLINED_PRE "| %7d | %7.3f | %14.3f | %13.3f | %9.3f |" ANSI_UNDERLINED_POST "\n", i+1, OEE, availability, performance, quality);
     }
 
     printf("\n\n");
@@ -50,10 +50,10 @@ void printResult3(int amount_of_processes, process processes[], manufacturing_sy
 
     printf("\n___________________________________\n");
     printf(ANSI_UNDERLINED_PRE "|             Table 3             |" ANSI_UNDERLINED_POST "\n");
-    printf(ANSI_UNDERLINED_PRE "| OEE          |  %3.3f  |" ANSI_UNDERLINED_POST "\n", OEE);
-    printf(ANSI_UNDERLINED_PRE "| Availability |  %3.3f  |" ANSI_UNDERLINED_POST "\n", availability_mean);
-    printf(ANSI_UNDERLINED_PRE "| Performance  |  %3.3f  |" ANSI_UNDERLINED_POST "\n", performance_mean);
-    printf(ANSI_UNDERLINED_PRE "| Quality      |  %3.3f  |" ANSI_UNDERLINED_POST "\n", quality_mean);
+    printf(ANSI_UNDERLINED_PRE "| OEE            |  %12.3f  |" ANSI_UNDERLINED_POST "\n", OEE);
+    printf(ANSI_UNDERLINED_PRE "| Availability   |  %12.3f  |" ANSI_UNDERLINED_POST "\n", availability_mean);
+    printf(ANSI_UNDERLINED_PRE "| Performance    |  %12.3f  |" ANSI_UNDERLINED_POST "\n", performance_mean);
+    printf(ANSI_UNDERLINED_PRE "| Quality        |  %12.3f  |" ANSI_UNDERLINED_POST "\n", quality_mean);
 
     printf("\n\nIdeal cycle time: %f \nTotal count: %d \nRun time: %f ", processes[0].ideal_cycle_time, manu_system.total_count, manu_system.planned_production_time - stops(processes[0], amount_of_processes));
 }
